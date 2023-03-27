35864ff404b73592babe32116b4e11f3fa92049d
Rachit Lour <rachitWD@gmail.com>
2023-03-27 22:18:26 +0530

    Removed a bug , due to which computer values were not update and computer choices were generated different in getComputerchoice and result function

--- script.js
+++ script.js
@@ -1,55 +1,47 @@
+// Computer Choice for Rock Paper Selection
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-  if (randomNumber === 1) {
-    return "rock";
-  } else if (randomNumber === 2) {
-    return "paper";
-  } else {
-    return "scissor";
-  }
+  return randomNumber;
 }
 
-function playRound(player, computer) {
-  // your code here!
-  if (player === "ROCK" && computer === "PAPER") {
-    return "Computer Won";
-  } else if (player === "ROCK" && computer === "SCISSOR") {
+console.log("Output after get computer function", `${getComputerChoice()}`);
+// ----* Computer selection for Rock paper Scissor
+
+function play(player, computer) {
+  if (player === 1 && computer === 2) {
+    return "You Lose";
+  } else if (player === 1 && computer === 3) {
     return "You Won";
-  } else if (player === "ROCK" && computer === "ROCK") {
-    return "Draw";
-  } else if (player === "PAPER" && computer === "ROCK") {
+  } else if (player === 2 && computer === 1) {
     return "You Won";
-  } else if (player === "PAPER" && computer === "SCISSOR") {
-    return "Computer Won";
-  } else if (player === "PAPER" && computer === "PAPER") {
-    return "draw";
-  } else if (player === "SCISSOR" && computer === "PAPER") {
-    return "Computer Won";
-  } else if (player === "SCISSOR" && computer === "ROCK") {
+  } else if (player === 2 && computer === 3) {
+    return "You Lose";
+  } else if (player === 3 && computer === 1) {
+    return "You Lose";
+  } else if (player === 3 && computer === 2) {
     return "You Won";
   } else {
     return "draw";
   }
 }
+let playerScore = 0;
+let computerScore = 0;
+function display(e) {
+  let playerSelection = Number(e.target.value);
+  console.log("Player Choice in display", playerSelection);
+  let computerSelection = getComputerChoice();
+  console.log("Computer choice in display function", computerSelection);
+  let roundResult = play(playerSelection, computerSelection);
+  console.log("Computer choice after roundresult function", computerSelection);
 
-function updateScores(result) {
-  let playerScore = 0;
-  let computerScore = 0;
-  if (result === "You Won") {
+  if (roundResult === "You Won") {
     playerScore++;
-  } else if (result === "Computer Won") {
+  } else if (roundResult === "You Lose") {
     computerScore++;
   }
-  console.log(playerScore, computerScore);
   document.getElementById("player-score").textContent = playerScore;
   document.getElementById("computer-score").textContent = computerScore;
 }
 
-function display(e) {
-  let playerSelection = e.target.textContent;
-  let roundResult = playRound(playerSelection, getComputerChoice());
-  updateScores(roundResult);
-}
 let choices = document.querySelectorAll(".btn");
 choices.forEach((choice) => choice.addEventListener("click", display));
